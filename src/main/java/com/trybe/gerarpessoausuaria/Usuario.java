ad831185d25b802f57aea3c5b7ab3f30057ca005
ricardo-sousa <rwmsousa@gmail.com>
2022-03-24 14:47:24 -0300

    feat - implementation challenge

--- src/main/java/com/trybe/gerarpessoausuaria/Usuario.java
+++ src/main/java/com/trybe/gerarpessoausuaria/Usuario.java
@@ -1,21 +1,21 @@
 package com.trybe.gerarpessoausuaria;
 
 public class Usuario extends Pessoa {
-  String nome;
-  String sobrenome;
 
   public Usuario(String nome, String sobrenome) {
-    super(nome, sobrenome);
-    this.nome = nome;
-    this.sobrenome = sobrenome;
+    super.nome = nome;
+    super.sobrenome = sobrenome;
   }
 
   /** Método para pegar o nome e sobrenome do usuário. */
   public String getUsuario() {
-    if ((nome == "" || nome == null) || (sobrenome == "" || sobrenome == null)) {
-      return "Usuário inválido";
-    } else {
-      return getNome() + "." + getSobrenome();
+    if (super.nome == null
+        || super.sobrenome == null
+        || super.nome.isEmpty()
+        || super.sobrenome.isEmpty()) {
+      String invalidUser = "Usuário inválido";
+      return invalidUser;
     }
+    return super.nome + "." + super.sobrenome;
   }
 }
